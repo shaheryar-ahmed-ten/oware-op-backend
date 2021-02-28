b5771b259af68575705db21013c3d78fd2071aa5
Yousha Rizvi <yousharizvi@gmail.com>
2021-02-28 16:42:12 +0500

    login API complete with setup

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,19 +11,40 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-    }
+      User.belongsTo(models.Role, {
+        foreignKey: 'role_id'
+      });
+    };
+    generateHash(password) {
+      return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    };
+    comparePassword(password) {
+      return bcrypt.compareSync(password, this.password);
+    };
   };
   User.init({
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     username: DataTypes.STRING,
     phone: DataTypes.STRING,
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     isActive: DataTypes.BOOLEAN,
   }, {
     sequelize,
     modelName: 'User',
   });
+  User.beforeCreate((user, options) => {
+    const hashedPassword = user.generateHash(user.password);
+    user.password = hashedPassword;
+  });
+
   return User;
 };
