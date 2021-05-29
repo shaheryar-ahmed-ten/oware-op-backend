e889b4a0835bb4299e40309aeea0fde28c23a9db
Yousha Rizvi <yousharizvi@gmail.com>
2021-05-29 19:32:28 +0500

    Create inbound and outbound views, change column types in products

---
+++ migrations/20210529094009-outbound-stats-view.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const viewName = 'OutboundStats';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+        CREATE VIEW ${viewName} AS
+        SELECT Inventories.customerId,
+          Inventories.warehouseId,
+          Inventories.productId,
+          Product.name AS product,
+          Product.weight AS weight,
+          Product.dimensionsCBM AS dimensionsCBM,
+          UOM.name AS uom,
+          Warehouse.name AS warehouse,
+          Customer.companyName AS customer,
+          DO.id AS dispatchOrderId,
+          PO.id AS productOutwardId,
+          DO.quantity AS dispatchOrderQuantity,
+          PO.quantity AS productOutwardQuantity,
+          DO.createdAt AS dispatchOrderCreatedAt,
+          PO.createdAt AS productOutwardCreatedAt
+        FROM ProductOutwards AS PO
+          LEFT JOIN DispatchOrders AS DO ON PO.dispatchOrderId = DO.id
+          LEFT JOIN Inventories ON DO.inventoryId = Inventories.id
+          LEFT JOIN Products AS Product ON Inventories.productId = Product.id
+          LEFT JOIN Warehouses AS Warehouse ON Inventories.warehouseId = Warehouse.id
+          LEFT JOIN Customers AS Customer ON Inventories.customerId = Customer.id
+          LEFT JOIN UOMs AS UOM ON Product.uomId = UOM.id;
+  `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`DROP VIEW ${viewName};`);
+  }
+};
