ac276c3d86d6bb7d1d95c7cdab94893d79067ef9
Yousha Rizvi <yousharizvi@gmail.com>
2021-06-14 17:21:35 +0500

    Linting and isActive check on employees

--- api/public.js
+++ api/public.js
@@ -27,32 +27,34 @@ router.post("/customer-inquiry", async (req, res, next) => {
 });
 
 router.get('/3478yr2387yrj23udnhiuefi', async (req, res, next) => {
-  let where = {
-  };
-  try{
-  const response = await Company.findAll({
-    attributes: ['id'],
-    include: [{ model: User, as: 'Employees', attributes: ['email'] }],
-  });
-  response.forEach(async (Company) => {
-    const customerObj = {
-     emails:await Company.Employees.map(Employee=>Employee.email),
-     data:await statisticsOfCustomer(Company.id)
-    }
-    const emails = customerObj.emails.toString()
-    const data = JSON.stringify(customerObj.data)
-    const subject = 'Weekly Notification'
-    const senderName = 'Oware Technologies'
-    sendGeneralEmailToCompanys(emails,data,subject,senderName)
-
-  });
-}catch(e){
-  res.json({
-    success: false,
-    message: 'Something Went Wrong',
-    data: e.message,
-  });
-}
+  try {
+    const response = await Company.findAll({
+      attributes: ['id'],
+      include: [{
+        model: User,
+        as: 'Employees',
+        attributes: ['email'],
+        where: { isActive: true }
+      }],
+    });
+    response.forEach(async (Company) => {
+      const customerObj = {
+        emails: await Company.Employees.map(Employee => Employee.email),
+        data: await statisticsOfCustomer(Company.id)
+      };
+      const emails = customerObj.emails.toString();
+      const data = JSON.stringify(customerObj.data);
+      const subject = 'Weekly Notification';
+      const senderName = 'Oware Technologies';
+      sendGeneralEmailToCompanys(emails, data, subject, senderName)
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      message: 'Something Went Wrong',
+      data: e.message,
+    });
+  }
   res.json({
     success: true,
     message: 'respond with a resource',
