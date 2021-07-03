e5339ffc9dc4897bee3c98c7b6be658e1eee04a6
fahamakhan <fahamak@yahoo.com>
2021-07-03 12:37:20 +0500

    presigned url working

--- services/s3.service.js
+++ services/s3.service.js
@@ -1,4 +1,5 @@
 const aws = require('aws-sdk');
+
 const {
   S3_UPLOAD_IAM_USER_SECRET,
   S3_UPLOAD_IAM_USER_KEY,
@@ -10,3 +11,12 @@ module.exports.s3 = new aws.S3({
   secretAccessKey: S3_UPLOAD_IAM_USER_SECRET,
   accessKeyId: S3_UPLOAD_IAM_USER_KEY,
 });
+
+module.exports.previewFile = async(Bucket,Key) => {
+  const url = await this.s3.getSignedUrl('getObject', {
+    Bucket,
+    Key,
+    ResponseContentType: 'image/png'
+  });
+  return url
+}
