1f7863bfaf6fa08a439ecc75267d3dc27548f8ac
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-08-06 18:22:22 +0500

    function used for search

--- api/productOutward.js
+++ api/productOutward.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   Inventory,
@@ -15,135 +15,156 @@ const {
   Product,
   UOM,
   sequelize
-} = require('../models')
-const config = require('../config');
+} = require("../models");
+const config = require("../config");
 const { Op, where } = require("sequelize");
-const { digitize } = require('../services/common.services');
-
+const { digitize } = require("../services/common.services");
 
 /* GET productOutwards listing. */
-router.get('/', async (req, res, next) => {
-  const limit = req.query.rowsPerPage || config.rowsPerPage
+router.get("/", async (req, res, next) => {
+  const limit = req.query.rowsPerPage || config.rowsPerPage;
   const offset = (req.query.page - 1 || 0) * limit;
   let where = {
     // userId: req.userId
   };
-  if (req.query.search) where[Op.or] = ['$Inventories.Product.name$', '$Inventories.Company.name$', '$Inventories.Warehouse.name$']
-    .map(key => ({ [key]: { [Op.like]: '%' + req.query.search + '%' } }));
+  if (req.query.search)
+    where[Op.or] = ["$Inventories.Product.name$", "$Inventories.Company.name$", "$Inventories.Warehouse.name$"].map(key => ({
+      [key]: { [Op.like]: "%" + req.query.search + "%" }
+    }));
   const response = await ProductOutward.findAndCountAll({
     duplicating: false,
     include: [
       {
         duplicating: false,
         model: DispatchOrder,
-        include: [{
-          model: Inventory,
-          as: 'Inventory',
-          include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
-        }, {
-          model: Inventory,
-          as: 'Inventories',
-          include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
-        }]
-      }, {
+        include: [
+          {
+            model: Inventory,
+            as: "Inventory",
+            include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
+          },
+          {
+            model: Inventory,
+            as: "Inventories",
+            include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
+          }
+        ]
+      },
+      {
         model: Vehicle,
         include: [{ model: Car, include: [CarMake, CarModel] }]
       },
       {
-        model: Inventory, as: 'Inventories',
+        model: Inventory,
+        as: "Inventories",
         include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
       }
     ],
-    order: [['updatedAt', 'DESC']],
+    order: [["updatedAt", "DESC"]],
     //subQuery: false,
-    where, limit, offset,
+    where,
+    limit,
+    offset
   });
-  var acc = []
+  var acc = [];
   response.rows.forEach(productOutward => {
-    var sum = []
-    productOutward.DispatchOrder.Inventories.forEach((Inventory) => {
-      sum.push(Inventory.OrderGroup.quantity)
-    })
-    acc.push(sum.reduce((acc, po) => {
-      return acc + po
-    }))
+    var sum = [];
+    productOutward.DispatchOrder.Inventories.forEach(Inventory => {
+      sum.push(Inventory.OrderGroup.quantity);
+    });
+    acc.push(
+      sum.reduce((acc, po) => {
+        return acc + po;
+      })
+    );
   });
   for (let index = 0; index < acc.length; index++) {
     response.rows[index].DispatchOrder.quantity = acc[index];
   }
 
-  var comittedAcc = []
+  var comittedAcc = [];
   response.rows.forEach(productOutward => {
-    var sumOfComitted = []
-    productOutward.Inventories.forEach((Inventory) => {
-      sumOfComitted.push(Inventory.OutwardGroup.quantity)
-    })
-    comittedAcc.push(sumOfComitted.reduce((acc, po) => {
-      return acc + po
-    }))
+    var sumOfComitted = [];
+    productOutward.Inventories.forEach(Inventory => {
+      sumOfComitted.push(Inventory.OutwardGroup.quantity);
+    });
+    comittedAcc.push(
+      sumOfComitted.reduce((acc, po) => {
+        return acc + po;
+      })
+    );
   });
   for (let index = 0; index < comittedAcc.length; index++) {
-    response.rows[index].quantity = comittedAcc[index]
+    response.rows[index].quantity = comittedAcc[index];
   }
 
   res.json({
     success: true,
-    message: 'respond with a resource',
+    message: "respond with a resource",
     data: response.rows,
     pages: Math.ceil(response.count / limit)
   });
 });
 
 /* POST create new productOutward. */
-router.post('/', async (req, res, next) => {
-  let message = 'New productOutward registered';
-  let dispatchOrder = await DispatchOrder.findByPk(req.body.dispatchOrderId, { include: [ProductOutward] })
-  if (!dispatchOrder) return res.json({
-    success: false,
-    message: 'No dispatch order found'
-  });
+router.post("/", async (req, res, next) => {
+  let message = "New productOutward registered";
+  let dispatchOrder = await DispatchOrder.findByPk(req.body.dispatchOrderId, { include: [ProductOutward] });
+  if (!dispatchOrder)
+    return res.json({
+      success: false,
+      message: "No dispatch order found"
+    });
   req.body.inventories = req.body.inventories || [{ id: req.body.inventoryId, quantity: req.body.quantity }];
 
   try {
     await sequelize.transaction(async transaction => {
-      productOutward = await ProductOutward.create({
-        userId: req.userId,
-        ...req.body
-      }, { transaction });
+      productOutward = await ProductOutward.create(
+        {
+          userId: req.userId,
+          ...req.body
+        },
+        { transaction }
+      );
       const numberOfInternalIdForBusiness = digitize(productOutward.id, 6);
       productOutward.internalIdForBusiness = req.body.internalIdForBusiness + numberOfInternalIdForBusiness;
       let sumOfOutwards = [];
       let outwardAcc;
-      req.body.inventories.forEach((Inventory) => {
+      req.body.inventories.forEach(Inventory => {
         let quantity = parseInt(Inventory.quantity);
         sumOfOutwards.push(quantity);
-      })
-      outwardAcc = (sumOfOutwards.reduce((acc, po) => {
-        return acc + po
-      }))
+      });
+      outwardAcc = sumOfOutwards.reduce((acc, po) => {
+        return acc + po;
+      });
       productOutward.quantity = outwardAcc;
       await productOutward.save({ transaction });
 
-      await OutwardGroup.bulkCreate(req.body.inventories.map(inventory => ({
-        userId: req.userId,
-        outwardId: productOutward.id,
-        inventoryId: inventory.id,
-        quantity: inventory.quantity
-      })), { transaction });
+      await OutwardGroup.bulkCreate(
+        req.body.inventories.map(inventory => ({
+          userId: req.userId,
+          outwardId: productOutward.id,
+          inventoryId: inventory.id,
+          quantity: inventory.quantity
+        })),
+        { transaction }
+      );
 
-      return Promise.all(req.body.inventories.map(_inventory => {
-        return Inventory.findByPk(_inventory.id, { transaction }).then(inventory => {
-          if (!inventory && !_inventory.id) throw new Error('Inventory is not available');
-          if (_inventory.quantity > inventory.committedQuantity) throw new Error('Cannot create orders above available quantity');
-          try {
-            inventory.dispatchedQuantity += (+_inventory.quantity);
-            inventory.committedQuantity -= (+_inventory.quantity);
-            return inventory.save({ transaction });
-          } catch (err) {
-            throw new Error(err.errors.pop().message);
-          }
-        });
-      }));
+      return Promise.all(
+        req.body.inventories.map(_inventory => {
+          return Inventory.findByPk(_inventory.id, { transaction }).then(inventory => {
+            if (!inventory && !_inventory.id) throw new Error("Inventory is not available");
+            if (_inventory.quantity > inventory.committedQuantity) throw new Error("Cannot create orders above available quantity");
+            try {
+              inventory.dispatchedQuantity += +_inventory.quantity;
+              inventory.committedQuantity -= +_inventory.quantity;
+              return inventory.save({ transaction });
+            } catch (err) {
+              throw new Error(err.errors.pop().message);
+            }
+          });
+        })
+      );
     });
     return res.json({
       success: true,
@@ -163,7 +184,7 @@ router.post('/', async (req, res, next) => {
   } catch (err) {
     res.json({
       success: false,
-      message: err.toString().replace('Error: ', '')
+      message: err.toString().replace("Error: ", "")
     });
   }
   res.json({
@@ -174,12 +195,13 @@ router.post('/', async (req, res, next) => {
 });
 
 /* PUT update existing productOutward. */
-router.put('/:id', async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   let productOutward = await ProductOutward.findOne({ where: { id: req.params.id }, include: [{ model: ProductInward }] });
-  if (!productOutward) return res.status(400).json({
-    success: false,
-    message: 'No productOutward found!'
-  });
+  if (!productOutward)
+    return res.status(400).json({
+      success: false,
+      message: "No productOutward found!"
+    });
   await productOutward.ProductInward.save();
   productOutward.receiverName = req.body.receiverName;
   productOutward.receiverPhone = req.body.receiverPhone;
@@ -188,7 +210,7 @@ router.put('/:id', async (req, res, next) => {
     const response = await productOutward.save();
     return res.json({
       success: true,
-      message: 'Product Outward updated',
+      message: "Product Outward updated",
       data: response
     });
   } catch (err) {
@@ -199,45 +221,157 @@ router.put('/:id', async (req, res, next) => {
   }
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   let response = await ProductOutward.destroy({ where: { id: req.params.id } });
-  if (response) res.json({
-    success: true,
-    message: 'ProductOutward deleted'
-  });
-  else res.status(400).json({
-    success: false,
-    message: 'No productOutward found!'
-  });
-})
-router.get('/relations', async (req, res, next) => {
+  if (response)
+    res.json({
+      success: true,
+      message: "ProductOutward deleted"
+    });
+  else
+    res.status(400).json({
+      success: false,
+      message: "No productOutward found!"
+    });
+});
+router.get("/relations", async (req, res, next) => {
   const dispatchOrders = await DispatchOrder.findAll({
-    include: [{
-      model: Inventory,
-      as: 'Inventory',
-      include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
-    }, {
-      model: Inventory,
-      as: 'Inventories',
-      include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
-    }, {
-      model: ProductOutward,
-      include: [{
-        model: Vehicle
-      }, {
-        model: Inventory, as: 'Inventories',
+    include: [
+      {
+        model: Inventory,
+        as: "Inventory",
         include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
-
-      }]
-    }],
-    order: [['updatedAt', 'DESC']]
+      },
+      {
+        model: Inventory,
+        as: "Inventories",
+        include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
+      },
+      {
+        model: ProductOutward,
+        include: [
+          {
+            model: Vehicle
+          },
+          {
+            model: Inventory,
+            as: "Inventories",
+            include: [{ model: Product, include: [{ model: UOM }] }, { model: Company }, { model: Warehouse }]
+          }
+        ]
+      }
+    ],
+    order: [["updatedAt", "DESC"]]
   });
   const vehicles = await Vehicle.findAll({ where: { isActive: true } });
   res.json({
     success: true,
-    message: 'respond with a resource',
-    dispatchOrders, vehicles
+    message: "respond with a resource",
+    dispatchOrders,
+    vehicles
   });
 });
+
+const makeFilterQuery = (whereClause, transform = {}) => {
+  for (let item in whereClause) {
+    if (whereClause[item] === "true") {
+      whereClause[item] = true;
+    } else if (whereClause[item] === "false") {
+      whereClause[item] = false;
+    } else if (!isNaN(Number(whereClause[item]))) {
+      whereClause[item] = Number(whereClause[item]);
+    }
+    if (typeof whereClause[item] === "string") {
+      whereClause[item] = { [Op.like]: "%" + whereClause[item] + "%" };
+    }
+  }
+  return whereClause;
+};
+
+const modelWiseFilters = (filters, modelname) => {
+  const filterObj = {};
+
+  for (const key in filters) {
+    // if(key.split(".")[1] != ('to' || 'from')){
+
+    // }
+    model = key.split(".")[0];
+    if (filterObj[model] == undefined) {
+      filterObj[model] = { [key.split(".")[1]]: filters[key] }; //if user model doesn't exist: users = {name:"shaheryar"}
+    } else {
+      filterObj[model][key.split(".")[1]] = filters[key]; //if user model exist: users.name = "shaheryar"
+    }
+  }
+
+  let obj = Object.keys(filterObj)
+    .map(function (key, index) {
+      if (key == modelname) {
+        return filterObj[key];
+      }
+    })
+    .filter(function (x) {
+      return x !== undefined;
+    })[0];
+
+  if (obj) {
+    const { to, from } = obj;
+    if (to && from) {
+      obj = removeFromObject(obj, ["to", "from"])[0];
+      obj["createdAt"] = {
+        [Op.gte]: moment().set("hour", 0).set("minute", 0).set("second", 0),
+        [Op.lte]: moment().set("hour", 0).set("minute", 0).set("second", 0)
+      };
+    }
+    return obj;
+  } else
+    return {
+      //include all elements
+      id: {
+        [Op.gt]: 0
+      }
+    };
+};
+
+const attachDateFilter = (filters, query) => {
+  const { to, from } = query;
+  if (to && from) {
+    createdAt = {
+      [Op.gte]: moment(from).toISOString(),
+      [Op.lte]: moment(to).add(1, "seconds").toISOString()
+    };
+    filters["createdAt"] = createdAt;
+  }
+  return filters;
+};
+
+const removeFromObject = (obj, keys = []) => {
+  if (Array.isArray(obj)) {
+    return Object.assign([], obj).map(item => {
+      keys.forEach(key => {
+        if (item.hasOwnProperty(key)) {
+          delete item[key];
+        }
+      });
+      return item;
+    });
+  } else {
+    return [Object.assign({}, obj)].map(item => {
+      keys.forEach(key => {
+        if (item.hasOwnProperty(key)) {
+          delete item[key];
+        }
+      });
+      return item;
+    });
+  }
+};
+
+const removeChildModelFilters = where => {
+  console.log("where1", where);
+  for (const key in where) {
+    if (key.includes(".")) delete where[key];
+  }
+  console.log("where2", where);
+};
 
 module.exports = router;
