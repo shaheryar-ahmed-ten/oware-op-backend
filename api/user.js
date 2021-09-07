e93d41577ce4cdb622f5569053ed79e8313d96d2
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-09-07 14:31:33 +0500

    add user bug resolved:companyId set to null if it is empty error

--- api/user.js
+++ api/user.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const { User, Role, PermissionAccess, Permission, Company } = require('../models')
-const config = require('../config');
-const { isLoggedIn, checkPermission, isSuperAdmin } = require('../services/auth.service');
+const { User, Role, PermissionAccess, Permission, Company } = require("../models");
+const config = require("../config");
+const { isLoggedIn, checkPermission, isSuperAdmin } = require("../services/auth.service");
 const { Op } = require("sequelize");
-const { PERMISSIONS, PORTALS, RELATION_TYPES } = require('../enums');
-const PORTALS_LABELS = require('../enums/portals');
+const { PERMISSIONS, PORTALS, RELATION_TYPES } = require("../enums");
+const PORTALS_LABELS = require("../enums/portals");
 
 async function updateUser(req, res, next) {
   let user = await User.findOne({ where: { id: req.params.id } });
-  if (!user) return res.status(400).json({
-    success: false,
-    message: 'No user found!'
-  });
+  if (!user)
+    return res.status(400).json({
+      success: false,
+      message: "No user found!"
+    });
   user.firstName = req.body.firstName;
   user.lastName = req.body.lastName;
   user.roleId = req.body.roleId;
@@ -25,7 +26,7 @@ async function updateUser(req, res, next) {
     const response = await user.save();
     return res.json({
       success: true,
-      message: 'User updated',
+      message: "User updated",
       data: response
     });
   } catch (err) {
@@ -37,48 +38,59 @@ async function updateUser(req, res, next) {
 }
 
 /* GET users listing. */
-router.get('/', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
-  const limit = req.query.rowsPerPage || config.rowsPerPage
+router.get("/", isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
+  const limit = req.query.rowsPerPage || config.rowsPerPage;
   const offset = (req.query.page - 1 || 0) * limit;
   let where = {};
-  if (req.query.search) where[Op.or] = ['firstName', 'lastName'].map(key => ({ [key]: { [Op.like]: '%' + req.query.search + '%' } }));
+  if (req.query.search)
+    where[Op.or] = ["firstName", "lastName"].map(key => ({ [key]: { [Op.like]: "%" + req.query.search + "%" } }));
   const response = await User.findAndCountAll({
     distinct: true,
-    include: [{
-      model: Role,
-      include: [{ model: PermissionAccess, include: [{ model: Permission }] }]
-    }, {
-      model: Company,
-      as: 'Company'
-    }],
-    order: [['updatedAt', 'DESC']],
-    limit, offset, where
+    include: [
+      {
+        model: Role,
+        include: [{ model: PermissionAccess, include: [{ model: Permission }] }]
+      },
+      {
+        model: Company,
+        as: "Company"
+      }
+    ],
+    order: [["updatedAt", "DESC"]],
+    limit,
+    offset,
+    where
   });
   res.json({
     success: true,
-    message: 'respond with a resource',
+    message: "respond with a resource",
     data: response.rows,
     pages: Math.ceil(response.count / limit)
   });
 });
 
 /* GET current logged in user. */
-router.get('/me', isLoggedIn, async (req, res, next) => {
+router.get("/me", isLoggedIn, async (req, res, next) => {
   return res.json({
     success: true,
     data: req.user
-  })
+  });
 });
 
 /* GET current logged in user. */
-router.put('/me', isLoggedIn, async (req, res, next) => {
-  req.params.id = req.userId;
-  return next()
-}, updateUser);
+router.put(
+  "/me",
+  isLoggedIn,
+  async (req, res, next) => {
+    req.params.id = req.userId;
+    return next();
+  },
+  updateUser
+);
 
 /* POST user login. */
-router.post('/auth/login', async (req, res, next) => {
-  let loginKey = req.body.username.indexOf('@') > -1 ? 'email' : 'username';
+router.post("/auth/login", async (req, res, next) => {
+  let loginKey = req.body.username.indexOf("@") > -1 ? "email" : "username";
   const user = await User.findOne({
     where: { [loginKey]: req.body.username },
     include: [Role]
@@ -86,38 +98,41 @@ router.post('/auth/login', async (req, res, next) => {
   if (!user)
     return res.status(401).json({
       success: false,
-      message: 'User doesn\'t exist with this email!'
+      message: "User doesn't exist with this email!"
     });
   let isPasswordValid = user.comparePassword(req.body.password);
-  if (!isPasswordValid) return res.status(401).json({
-    success: false,
-    message: 'Invalid password!'
-  });
-  if (user.Role.allowedApps.split(',').indexOf(PORTALS.OPERATIONS) < 0)
+  if (!isPasswordValid)
+    return res.status(401).json({
+      success: false,
+      message: "Invalid password!"
+    });
+  if (user.Role.allowedApps.split(",").indexOf(PORTALS.OPERATIONS) < 0)
     return res.status(401).json({
       status: false,
-      message: 'Not allowed to enter operations portal'
+      message: "Not allowed to enter operations portal"
     });
   var token = jwt.sign({ id: user.id }, config.JWT_SECRET);
   res.json({
     success: isPasswordValid,
-    message: 'Login successful',
+    message: "Login successful",
     token
   });
 });
 
 /* POST create new user. */
-router.post('/', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
-  const adminRole = await Role.findOne({ where: { type: 'admin' } });
-  let message = 'New user registered';
+router.post("/", isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
+  const adminRole = await Role.findOne({ where: { type: "admin" } });
+  let message = "New user registered";
   let user;
   try {
+    if (req.body["companyId"] === "") req.body["companyId"] = null;
     user = await User.create({
       roleId: adminRole.id,
       ...req.body
     });
     user.password = undefined;
   } catch (err) {
+    console.log("err", err);
     return res.json({
       success: false,
       message: err.errors.pop().message
@@ -131,22 +146,23 @@ router.post('/', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (
 });
 
 /* PUT update existing user. */
-router.put('/:id', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), updateUser);
+router.put("/:id", isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), updateUser);
 
-router.delete('/:id', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
+router.delete("/:id", isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
   let response = await User.destroy({ where: { id: req.params.id } });
-  if (response) res.json({
-    success: true,
-    message: 'User deleted'
-  });
-  else res.status(400).json({
-    success: false,
-    message: 'No user found!'
-  });
-})
+  if (response)
+    res.json({
+      success: true,
+      message: "User deleted"
+    });
+  else
+    res.status(400).json({
+      success: false,
+      message: "No user found!"
+    });
+});
 
-
-router.get('/relations', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
+router.get("/relations", isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL), async (req, res, next) => {
   const roles = await Role.findAll();
   const portals = Object.keys(PORTALS_LABELS).map(portal => ({ id: portal, label: PORTALS_LABELS[portal] }));
   let where = {};
@@ -154,8 +170,10 @@ router.get('/relations', isLoggedIn, checkPermission(PERMISSIONS.OPS_USER_FULL),
   const customers = await Company.findAll({ where: { ...where, relationType: RELATION_TYPES.CUSTOMER } });
   res.json({
     success: true,
-    message: 'respond with a resource',
-    roles, customers, portals
+    message: "respond with a resource",
+    roles,
+    customers,
+    portals
   });
 });
 
