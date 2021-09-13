b3e127b7587abf4cd3cd43efadbb884c05b52fc0
HunbalSiddiqui <hunbal.dha@gmail.com>
2021-09-13 16:19:52 +0500

    get single vehicle type 1/2.

--- api/vehicleType/controller.js
+++ api/vehicleType/controller.js
@@ -6,15 +6,33 @@ exports.getVehicleTypes = async (params) => {
         const response = await VehicleType.findAndCountAll(params);
         if (response.count) {
             return {
+                status: httpStatus.OK,
                 success: true,
                 message: "Data Found",
                 data: response.rows,
                 pages: Math.ceil(response.count / params.limit)
             };
         }
-        else return { success: false, message: "Data not Found", data: [], count: response.count };
+        else return { success: false, status: httpStatus.OK, message: "Data not Found", data: [], count: response.count };
     }
     catch (err) {
+        console.log("ERROR:", err);
+        return {
+            success: false,
+            status: httpStatus.CONFLICT,
+            message: err.message,
+            code: "Failed to get data"
+        };
+    }
+}
+
+
+exports.getVehicleTypeById = async (params) => {
+    try {
+        const response = await VehicleType.findOne(params);
+        if (response) return { success: false, status: httpStatus.OK, message: "Data Found", data: response };
+        else return { status: httpStatus.OK, message: "Data not Found", data: [] };
+    } catch (err) {
         console.log("ERROR:", err);
         return {
             success: false,
--- api/vehicleType/routes.js
+++ api/vehicleType/routes.js
@@ -6,6 +6,7 @@ const { VehicleType } = require('../../models')
 const { Op } = require("sequelize");
 const moment = require("moment");
 
+// Vehicle type listing
 router.get('/', async (req, res) => {
     const limit = req.query.rowsPerPage || config.rowsPerPage
     const offset = (req.query.page - 1 || 0) * limit;
@@ -14,10 +15,24 @@ router.get('/', async (req, res) => {
         limit,
         offset,
         where,
+
         order: [['updatedAt', 'DESC']],
     }
     const response = await controller.getVehicleTypes(params)
-    res.sendJson(response.data, response.message, response.success, response.pages)
+    if (response.status === httpStatus.OK) res.sendJson(response.data, response.message, response.success, response.pages)
+    else res.sendError(response.status, response.message, response.error);
+
+})
+
+// single vehicle type
+router.get("/:id", async (req, res) => {
+    let where = { id: req.params.id };
+    const params = {
+        where
+    }
+    const response = await controller.getVehicleTypeById(params)
+    if (response.status === httpStatus.OK) res.sendJson(response.data, response.message, response.success);
+    else res.sendError(response.status, response.message, response.error);
 })
 
 module.exports = router;
