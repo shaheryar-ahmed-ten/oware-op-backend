e95e3d6540024efddbba62f0cc55878743592ada
HunbalSiddiqui <hunbal.dha@gmail.com>
2021-09-17 16:56:10 +0500

    added types in vehicles relations api.

--- api/vehicle.js
+++ api/vehicle.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Vehicle, Driver, Car, CarMake, CarModel, Company, File } = require("../models");
+const { Vehicle, Driver, Car, CarMake, CarModel, Company, File, VehicleType } = require("../models");
 const config = require("../config");
 const { Op } = require("sequelize");
 const VEHICLE_TYPES = require("../enums/vehicleTypes");
@@ -121,7 +121,11 @@ router.get("/relations", async (req, res, next) => {
   });
   let cars = await Car.findAll({
     where,
-    include: [CarMake, CarModel],
+    include: [CarMake, CarModel, {
+      model: VehicleType,
+      as: 'VehicleType',
+      required: true,
+    }],
   });
 
   const vehicleTypes = VEHICLE_TYPES;
