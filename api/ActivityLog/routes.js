a62444fdb0f4bcbe2c96c41e9b26385eeaaaadf9
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-09-22 19:14:57 +0500

    days bug resolved

--- api/ActivityLog/routes.js
+++ api/ActivityLog/routes.js
@@ -20,9 +20,13 @@ router.get("/", async (req, res) => {
       [key]: { [Op.like]: "%" + req.query.search + "%" },
     }));
   if (req.query.days) {
-    const currentDate = moment();
-    const previousDate = moment().subtract(req.query.days, "days");
-    where["createdAt"] = { [Op.between]: [previousDate, currentDate] };
+    if (req.query.days == "1") {
+      where["createdAt"] = { [Op.eq]: currentDate };
+    } else {
+      const currentDate = moment();
+      const previousDate = moment().subtract(req.query.days, "days");
+      where["createdAt"] = { [Op.between]: [previousDate, currentDate] };
+    }
   }
   const params = {
     limit,
