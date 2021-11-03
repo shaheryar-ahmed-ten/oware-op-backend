08515e4ee61dfb373c5a9786284d4a9de3f44486
muhammadhamza703 <muhammad.hamza@experia.ai>
2021-11-03 10:06:41 +0500

    ETA Migration changed to Integer

--- migrations/20211101072816-modify_ride_current_location_eta_columns.js
+++ migrations/20211101072816-modify_ride_current_location_eta_columns.js
@@ -3,19 +3,27 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('Rides', 'eta', {
-      type: Sequelize.TIME,
+      type: Sequelize.INTEGER,
       allowNull: true,
     },
   );
   await queryInterface.changeColumn('Rides', 'completionTime', {
-      type: Sequelize.TIME,
+      type: Sequelize.INTEGER,
       allowNull: true,
     },
   );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Rides", "eta");
-    await queryInterface.removeColumn("Rides", "completionTime");
+    await queryInterface.changeColumn('Rides', 'eta', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+  );
+  await queryInterface.changeColumn('Rides', 'completionTime', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+  );
   }
 };
