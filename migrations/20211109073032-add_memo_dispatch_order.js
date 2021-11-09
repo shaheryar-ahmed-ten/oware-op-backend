3d1aa77d18c902f42120e32f27b37ce363ec3287
Muhammad Hamza <muhammad.hamza@experia.ai>
2021-11-09 12:33:38 +0500

    Migration Order Memo added

---
+++ migrations/20211109073032-add_memo_dispatch_order.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("DispatchOrders", "orderMemo", {
+      type: Sequelize.TEXT,
+      allowNull: true
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('DispatchOrders', 'orderMemo');
+  }
+};
