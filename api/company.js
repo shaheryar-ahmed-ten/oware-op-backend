f951559a8a61c7969348ae92e636c79807f858d7
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-11-17 18:48:57 +0500

    poc user bug fixes

--- api/company.js
+++ api/company.js
@@ -99,8 +99,19 @@ router.put("/:relationType/:id", activityLog, async (req, res, next) => {
   customer.isActive = req.body.isActive;
   customer.logoId = req.body.logoId;
   customer.phone = req.body.phone;
-  customer.pocUserId = req.body.pocUserId;
+  if (req.body.pocUserId) customer.pocUserId = req.body.pocUserId;
   try {
+    console.log(
+      "req.params.relationType",
+      req.params.relationType,
+      "req.body.isActive",
+      req.body.isActive,
+      "req.body.pocUserId",
+      req.body.pocUserId
+    );
+    if (req.params.relationType === "CUSTOMER" && req.body.isActive === true && !req.body.pocUserId) {
+      throw new Error("Please add a POC user");
+    }
     const response = await customer.save();
     await addActivityLog(req["activityLogId"], response, Dao.ActivityLog);
     return res.json({
@@ -109,9 +120,10 @@ router.put("/:relationType/:id", activityLog, async (req, res, next) => {
       data: response,
     });
   } catch (err) {
+    console.log("err", err.message);
     return res.json({
       success: false,
-      message: err.errors.pop().message,
+      message: err.message,
     });
   }
 });
